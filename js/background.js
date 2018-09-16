4d94cc7a1726bbe544ded07ed703e82dd5716523
joaonetoo <joaoantonioneto@outlook.com>
2018-09-16 15:48:22 -0300

    listerning para verificar quando a url for atualizada

---
+++ js/background.js
@@ -0,0 +1,18 @@
+var old_url 
+var current_url
+chrome.tabs.onUpdated.addListener(
+  function(tabId, changeInfo, tab) {
+    if(!current_url){
+      current_url = tab.url
+    }
+  
+  }
+);
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if(request.teste != current_url){
+      current_url = request.teste
+      sendResponse({change: true});
+    }
+    sendResponse({change: false});
+  });
--- js/popup.js
+++ js/popup.js
@@ -1,80 +1,81 @@
+
 var url
 chrome.tabs.getSelected(null,function(tab) {
     url = tab.url;
 });
 
-var notices
-$(document).ready(function(){
-    chrome.tabs.onUpdated.addListener(function() {
+chrome.runtime.sendMessage({ teste: url}, function(response) {
+    if (response.change){
         chrome.storage.sync.set({'news': 'false'}, function() {
             $('#notice').remove();
             $("#button_notices").show();
             $('.main').append('<div id=notice></div>');
-
         });
-        }
-    );    
-    chrome.storage.sync.get('news',function(data){
-        notices = data.news
-        if (notices == 'false'){
-            $( "#button_notices" ).click(function() {
-                $("#button_notices").hide();
-                $( ".main" ).append( '<div class="cssload-container"> </div>');
-                $( ".cssload-container" ).append( '<div class="cssload-whirlpool"></div>');
-    
-                    $.ajax({
-                        url : "http://206.189.215.95/notices",
-                        type : 'get',
-                        data : {
-                            site : url
-                        }
-                    })
-                    .done(function(msg){
-                        notices = msg['url']
-                        chrome.storage.sync.set({'news': notices}, function() {
-                        });
-                        $(".cssload-container").hide();
-                        if (notices.length > 0 ){
-                            $('#notice').append('<ul> </ul>');                
-                            notices.forEach(value => {
-                                $('#notice ul').append(
-                                    $('<li>').append(
-                                        $('<a>').attr('href',value[1]).append(
-                                            value[0]
+    }
+    var notices
+    $(document).ready(function(){ 
+        chrome.storage.sync.get('news',function(data){
+            notices = data.news
+            if (notices == 'false'){
+                $( "#button_notices" ).click(function() {
+                    $("#button_notices").hide();
+                    $( ".main" ).append( '<div class="cssload-container"> </div>');
+                    $( ".cssload-container" ).append( '<div class="cssload-whirlpool"></div>');
+        
+                        $.ajax({
+                            url : "http://206.189.215.95/notices",
+                            type : 'get',
+                            data : {
+                                site : url
+                            }
+                        })
+                        .done(function(msg){
+                            notices = msg['url']
+                            chrome.storage.sync.set({'news': notices}, function() {
+                            });
+                            $(".cssload-container").hide();
+                            if (notices.length > 0 ){
+                                $('#notice').append('<ul> </ul>');                
+                                notices.forEach(value => {
+                                    $('#notice ul').append(
+                                        $('<li>').append(
+                                            $('<a>').attr('href',value[1]).append(
+                                                value[0]
+                                            )
                                         )
                                     )
+                                });
+                            }else{
+                                $('#notice').append('<p> Não foram encontradas notícias parecidas! </p>' )
+                            }
+                        })
+                        .fail(function(jqXHR, textStatus, msg){
+                            $('#test').html(JSON.stringify(msg))
+                        }); 
+                
+            }); 
+            }else{
+                if (notices.length > 0 ){
+                    $("#button_notices").hide();
+                    $('#notice').append('<ul> </ul>');                
+                    notices.forEach(value => {
+                        $('#notice ul').append(
+                            $('<li>').append(
+                                $('<a>').attr('href',value[1]).append(
+                                    value[0]
                                 )
-                            });
-                        }else{
-                            $('#notice').append('<p> Não foram encontradas notícias parecidas! </p>' )
-                        }
-                    })
-                    .fail(function(jqXHR, textStatus, msg){
-                        $('#test').html(JSON.stringify(msg))
-                    }); 
-            
-        }); 
-        }else{
-            if (notices.length > 0 ){
-                $("#button_notices").hide();
-                $('#notice').append('<ul> </ul>');                
-                notices.forEach(value => {
-                    $('#notice ul').append(
-                        $('<li>').append(
-                            $('<a>').attr('href',value[1]).append(
-                                value[0]
                             )
                         )
-                    )
-                });
+                    });
 
-            
-            }else{
-                $('#notice').append('<p> Não foram encontradas notícias parecidas! </p>' )
-            }
-        } 
+                
+                }else{
+                    $('#notice').append('<p> Não foram encontradas notícias parecidas! </p>' )
+                }
+            } 
+        });
+    
     });
-   
 
-});
+  });
 
