57eeb1136c2ffea4ed76811f1671e416944a6990
joaonetoo <joaoantonioneto@outlook.com>
2018-10-13 01:24:34 -0300

    bug fix

--- js/background.js
+++ js/background.js
@@ -1,19 +1,19 @@
-var old_url 
-var current_url
-chrome.tabs.onUpdated.addListener(
-  function(tabId, changeInfo, tab) {
-    if(!current_url){
-      current_url = tab.url
-    }
+// var old_url 
+// var current_url
+// chrome.tabs.onUpdated.addListener(
+//   function(tabId, changeInfo, tab) {
+//     if(!current_url){
+//       current_url = tab.url
+//     }
   
-  }
-);
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    if(request.teste != current_url){
-      current_url = request.teste
-      sendResponse({change: true});
-    }
-    sendResponse({change: false});
-  });
+//   }
+// );
+// chrome.runtime.onMessage.addListener(
+//   function(request, sender, sendResponse) {
+//     if(request.teste != current_url){
+//       current_url = request.teste
+//       sendResponse({change: true});
+//     }
+//     sendResponse({change: false});
+//   });
 
--- js/popup.js
+++ js/popup.js
@@ -24,66 +24,68 @@ function createCard(titulo,link,image,date){
     '</a>'
     return html
 }
-chrome.runtime.sendMessage({ teste: url}, function(response) {
-    if (response.change){
-        chrome.storage.sync.set({'news': 'false'}, function() {
-            $('.pen-wrapper').remove();
-            $("#button_notices").show();
-            $('.main').append('<div class="pen-wrapper"></div>');
-        });
-    }
+function init () {
     var notices
-    $(document).ready(function(){ 
-        chrome.storage.sync.get('news',function(data){
-            notices = data.news
-            if (notices == 'false'){
-                $( "#button_notices" ).click(function() {
-                    $("#button_notices").hide();
-                    $( ".main" ).append( '<div class="cssload-container"> </div>');
-                    $( ".cssload-container" ).append( '<div class="cssload-whirlpool"></div>');
-                        $.ajax({
-                            url : "http://206.189.215.95/notices",
-                            type : 'get',
-                            data : {
-                                site : url
-                            }
-                        })
-                        .done(function(msg){
-                            notices = msg['url']
-                            chrome.storage.sync.set({'news': notices}, function() {
-                            });
-                            $(".cssload-container").hide();
-                            if (notices.length > 0 ){                
-                                notices.forEach(value => {
-                                    site = website(value[2])
-                                    html = createCard(value[0],value[1],site,value[3])
-                                    $('.pen-wrapper').append(html);
-                                    });
-                            }else{
-                                $('#notice').append('<p> Não foram encontradas notícias parecidas! </p>' )
-                            }
-                        })
-                        .fail(function(jqXHR, textStatus, msg){
-                            $('#test').html(JSON.stringify(msg))
-                        }); 
-                
-            }); 
+    chrome.storage.sync.get('news',function(data){
+        notices = data.news
+        if (notices === false || notices === undefined){
+            $( "#button_notices" ).click(function() {
+                $("#button_notices").hide();
+                $( ".main" ).append( '<div class="cssload-container"> </div>');
+                $( ".cssload-container" ).append( '<div class="cssload-whirlpool"></div>');
+                    $.ajax({
+                        url : "http://206.189.215.95/notices",
+                        type : 'get',
+                        data : {
+                            site : url
+                        }
+                    })
+                    .done(function(msg){
+                        notices = msg['url']
+                        chrome.storage.sync.set({'news': notices}, function() {
+                        });
+                        $(".cssload-container").hide();
+                        if (notices.length > 0 ){
+                            $("#clean").show();                
+                            notices.forEach(value => {
+                                site = website(value[2])
+                                html = createCard(value[0],value[1],site,value[3])
+                                $('.pen-wrapper').append(html);
+                                });
+                        }else{
+                            $('#notice').append('<p> Não foram encontradas notícias parecidas! </p>' )
+                        }
+                    })
+                    .fail(function(jqXHR, textStatus, msg){
+                        $('#test').html(JSON.stringify(msg))
+                    }); 
+            
+        }); 
+        }else{
+            if (notices.length > 0 ){
+                $("#button_notices").hide();
+                $("#clean").show();
+                notices.forEach(value => {
+                    site = website(value[2])
+                    html = createCard(value[0],value[1],site,value[3])  
+                    $('.pen-wrapper').append(html);
+                });
+            
             }else{
-                if (notices.length > 0 ){
-                    $("#button_notices").hide();
-                    notices.forEach(value => {
-                        site = website(value[2])
-                        html = createCard(value[0],value[1],site,value[3])  
-                        $('.pen-wrapper').append(html);
-                    });
-                
-                }else{
-                    $('#notice').append('<p> Não foram encontradas notícias parecidas! </p>' )
-                }
-            } 
-        });
-    
+                $('#notice').append('<p> Não foram encontradas notícias parecidas! </p>' )
+            }
+        } 
+        $("#clean").click(function(){
+            chrome.storage.sync.set({'news': false}, function() {
+                $("#clean").hide();
+                $(".pen-wrapper").html('');
+                $("#button_notices").show();
+                init();
+            });
+        })
     });
 
-  });
+}
+init();
+
 
